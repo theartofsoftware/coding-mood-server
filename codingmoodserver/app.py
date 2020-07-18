d5281fc650cf13b3ae7f6fdddbddcb490aa296d1
Mikey Mo <mikeyjkmo@gmail.com>
2020-07-18 11:56:41 +0100

    Refactor into separate service class

--- codingmoodserver/app.py
+++ codingmoodserver/app.py
@@ -1,79 +1,28 @@
-import re
 import logging
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
-from starlette.websockets import WebSocketDisconnect
+from starlette.responses import HTMLResponse
 import uvicorn
 
 from websocket_manager import WebsocketManager
+from coding_mood_service import CodingMoodService
 
 LOGGER = logging.getLogger(__name__)
-
-WORKING_STATE = {"state": "working"}
-BROKEN_STATE = {"state": "broken"}
-FIXING_STATE = {"state": "fixing"}
-
-
-def extract_number_of_failures(test_run_output):
-    num_of_failures = 0
-    failures_match = re.search(r"(\d+) failed", test_run_output)
-    if failures_match:
-        num_of_failures = int(failures_match.group(1))
-    return num_of_failures
-
-
-def get_new_state(last_num_of_failures, num_of_failures):
-    if num_of_failures == 0:
-        return WORKING_STATE
-    elif num_of_failures > last_num_of_failures:
-        return BROKEN_STATE
-    elif num_of_failures < last_num_of_failures:
-        return FIXING_STATE
-    return None
 
 
 def create_app():
     app = Starlette()
-    websocket_mgr = WebsocketManager()
-
-    LAST_NUM_OF_FAILURES = 0
-    LAST_STATE = None
+    coding_mood_service = CodingMoodService(WebsocketManager())
 
     @app.route('/state', methods=("POST",))
     async def set_new_state(request):
-        nonlocal LAST_NUM_OF_FAILURES
-        nonlocal LAST_STATE
-
         raw_body = await request.body()
         data = raw_body.decode("utf-8")
-
-        num_of_failures = extract_number_of_failures(data)
-        new_state = get_new_state(LAST_NUM_OF_FAILURES, num_of_failures)
-
-        if new_state and LAST_STATE != new_state:
-            LOGGER.info("Broadcasting new state: %s", new_state)
-            await websocket_mgr.broadcast(new_state)
-
-        LAST_NUM_OF_FAILURES = num_of_failures
-        LAST_STATE = new_state
-
-        return JSONResponse({})
+        await coding_mood_service.set_new_state(data)
+        return HTMLResponse()
 
     @app.websocket_route('/ws')
     async def websocket_endpoint(websocket):
-        await websocket.accept()
-        LOGGER.info("websocket opened")
-        websocket_mgr.register(websocket)
-        try:
-            while True:
-                mesg = await websocket.receive_text()
-                LOGGER.info("Received message", mesg)
-        except WebSocketDisconnect:
-            LOGGER.info("Websocket disconnected")
-        finally:
-            websocket_mgr.unregister(websocket)
-            await websocket.close()
-            LOGGER.info("websocket closed")
+        await coding_mood_service.handle_websocket(websocket)
 
     return app
 
---
+++ codingmoodserver/coding_mood_service.py
@@ -0,0 +1,61 @@
+import re
+import logging
+
+from starlette.websockets import WebSocketDisconnect
+
+LOGGER = logging.getLogger(__name__)
+
+WORKING_STATE = {"state": "working"}
+BROKEN_STATE = {"state": "broken"}
+FIXING_STATE = {"state": "fixing"}
+
+
+def extract_number_of_failures(test_run_output):
+    num_of_failures = 0
+    failures_match = re.search(r"(\d+) failed", test_run_output)
+    if failures_match:
+        num_of_failures = int(failures_match.group(1))
+    return num_of_failures
+
+
+def get_new_state(last_num_of_failures, num_of_failures):
+    if num_of_failures == 0:
+        return WORKING_STATE
+    elif num_of_failures > last_num_of_failures:
+        return BROKEN_STATE
+    elif num_of_failures < last_num_of_failures:
+        return FIXING_STATE
+    return None
+
+
+class CodingMoodService:
+    def __init__(self, websocket_mgr):
+        self.last_num_of_failures = 9
+        self.last_state = None
+        self.websocket_mgr = websocket_mgr
+
+    async def set_new_state(self, test_run_output):
+        num_of_failures = extract_number_of_failures(test_run_output)
+        new_state = get_new_state(self.last_num_of_failures, num_of_failures)
+
+        if new_state and self.last_state != new_state:
+            LOGGER.info("Broadcasting new state: %s", new_state)
+            await self.websocket_mgr.broadcast(new_state)
+
+        self.last_num_of_failures = num_of_failures
+        self.last_state = new_state
+
+    async def handle_websocket(self, websocket):
+        await websocket.accept()
+        LOGGER.info("websocket opened")
+        self.websocket_mgr.register(websocket)
+        try:
+            while True:
+                mesg = await websocket.receive_text()
+                LOGGER.debug("Received message", mesg)
+        except WebSocketDisconnect:
+            LOGGER.info("Websocket disconnected")
+        finally:
+            self.websocket_mgr.unregister(websocket)
+            await websocket.close()
+            LOGGER.info("websocket closed")
